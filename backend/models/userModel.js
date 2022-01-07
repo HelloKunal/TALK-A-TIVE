1831e46b72f4c8d1a167a3220b4f1b873769d452
Kunal <hellokunalpawar@gmail.com>
2022-01-07 22:40:42 +0530

    working on user authentication

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose')
+const bcryptjs = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
 	name:{type:String, required: true},
-	email:{type:String, required: true},
+	email:{type:String, required: true, unique: true},
 	password:{type:String, required: true},
-	pic:{type:String, required: true, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"},
+	pic:{type:String, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"},
 },
 	{
 		timestamps: true
 	}
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcryptjs.compare(enteredPassword, this.password);
+}
+
+userSchema.pre('save', async function(next) {
+	if(!this.isModified) {
+		next();
+	}
+
+	const salt = await bcryptjs.genSalt(10);
+	this.password = await bcryptjs.hash(this.password, salt);
+})
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDB = require("./config/db");
 const { chats } = require("./data/data");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+
+dotenv.config();
+connectDB();
 
 const app = express()
-dotenv.config();
+app.use(express.json()); // to accept json data
 
 app.get('/', (req, res) => {
 	res.send("API is Running Successfully");
 });
 
-app.get('/api/chat', (req, res) => {
-	res.send(chats);
-});
+app.use('/api/user', userRoutes)
 
-app.get('/api/chat/:id', (req, res) => {
-	// console.log(req.params.id);
-	const singleChat = chats.find((c) => c._id === req.params.id);
-	res.send(singleChat);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server Started on PORT ${PORT}`.red.bold));
